22f1f39a2a7e830e0f6b257554f724ec75b9e67b
unknown <ahmedalvi83@gmail.com>
2018-10-02 12:35:43 -0700

    a

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(BirdayEmails::class)->everyMinute();
+        $schedule->command(Commands\BirdayEmails::class)->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
